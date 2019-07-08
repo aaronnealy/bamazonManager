9e6ff943051a6df7f134f8aebe2ed037f93945fb
Aaron Nealy <50381328+aaronnealy@users.noreply.github.com>
2019-07-08 18:57:58 -0500

    set up mysql database connection, install required packages, and created a gitignore file.

---
+++ bamazonManager.js
@@ -0,0 +1,22 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var table = require("table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Dovedov9",
+    database: "bamazonManager_DB"
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
